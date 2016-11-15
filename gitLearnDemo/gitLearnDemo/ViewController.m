f5fb5547c6a7471e37f9f0830464c82e83382b8a
阿福-工作 <LinYongfu-work@chunni.local>
2016-11-15 15:14:24 +0800

    1.开始上传。

---
+++ gitLearnDemo/gitLearnDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  gitLearnDemo
+//
+//  Created by 阿福-工作 on 2016/11/15.
+//  Copyright © 2016年 阿福-工作. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 从本地上传到git仓库
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
